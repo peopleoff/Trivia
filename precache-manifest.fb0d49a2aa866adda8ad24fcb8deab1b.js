2b213f06639ccfb5e45c6bf5e0635b4114807efe
Ruslan Belyy <rbelyy@intruity.com>
2019-06-11 10:47:28 -0400

    deploy

---
+++ precache-manifest.fb0d49a2aa866adda8ad24fcb8deab1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Trivia/robots.txt"
+  },
+  {
+    "revision": "55fe57360026b2f31b87",
+    "url": "/Trivia/js/chunk-vendors.74a0036d.js"
+  },
+  {
+    "revision": "9c8e66555c50bf610015",
+    "url": "/Trivia/js/app.a3fb97d8.js"
+  },
+  {
+    "revision": "16b115b68d70a3e538c2",
+    "url": "/Trivia/js/about.30f5f3e5.js"
+  },
+  {
+    "revision": "227c6f7dabe5d7f7ee9e43f529b7be25",
+    "url": "/Trivia/index.html"
+  },
+  {
+    "revision": "55fe57360026b2f31b87",
+    "url": "/Trivia/css/chunk-vendors.5b032e5f.css"
+  }
+];
